3333f3f0c1ff15b57d7f4ea26740934579e56d1d
James Andrews <me@jamesandre.ws>
2008-06-21 21:14:26 +0100

    wikipedia updates

--- lib/wikipedia.rb
+++ lib/wikipedia.rb
@@ -1,17 +1,18 @@
 def wikipedia_data_for(word)
   doc = Hpricot(open("http://en.wikipedia.org/wiki/Special:Search?search=#{word}&fulltext=Search"))
+  data = { :description => doc.at("//div[@class='bodyContent]/p") }
+  
   if doc.at("//span[@class='latitude']")
-    { 
+    data.merge!({ 
       :type => 'location',
       :lat => doc.at("//span[@class='latitude']"), 
       :long => doc.at("//span[@class='longitude']"), 
-      :description => doc.at("//div[@class='bodyContent]/p")
-    }
+    })
   elseif doc.at("//span[@class='bday']")
-    {
+  data.merge!({ 
       :type => 'person',
       :bday => doc.at("//span[@class='bday']")
-    }
+    })
   end
 rescue
   nil
