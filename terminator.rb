d039e25c7127a776266ccb6612776d2f8772e814
James Andrews <me@jamesandre.ws>
2008-06-21 20:59:59 +0100

    wikipedia searching

--- terminator.rb
+++ terminator.rb
@@ -87,6 +87,25 @@ rescue
   nil
 end
 
+def wikipedia_data_for(word)
+  doc = Hpricot(open("http://en.wikipedia.org/wiki/Special:Search?search=#{word}&fulltext=Search"))
+  if doc.at("//span[@class='latitude']")
+    { 
+      :type => 'location',
+      :lat => doc.at("//span[@class='latitude']"), 
+      :long => doc.at("//span[@class='longitude']"), 
+      :description => doc.at("//div[@class='bodyContent]/p")
+    }
+  elseif doc.at("//span[@class='bday']")
+    {
+      :type => 'person',
+      :bday => doc.at("//span[@class='bday']")
+    }
+  end
+rescue
+  nil
+end
+
 def show_flickr_images(word)
   image1, image2, image3 = flickr_urls_for(word)
 
