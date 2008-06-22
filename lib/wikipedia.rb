5bec92fc97ec197552f63f7882650387dbc3377c
James Andrews <me@jamesandre.ws>
2008-06-22 11:24:23 +0100

    further wikipedia enhancements

--- lib/wikipedia.rb
+++ lib/wikipedia.rb
@@ -1,19 +1,19 @@
 require 'rubygems'
-require 'hpricot'
 require 'open-uri'
+require 'hpricot'
 
 def wikipedia_data_for(word)
   doc = Hpricot(open("http://en.wikipedia.org/wiki/Special:Search?search=#{word}"))
-  data = { :name => (doc/"h1.firstHeading").first.inner_html }
-  #, :description => (doc/"div.bodyContent/p").first.inner_html
-  unless (doc/"span.latitude").nil?
+  data = { :name => (doc/"h1.firstHeading").first.inner_html, :description => (doc/"div#bodyContent/p").first.inner_html }
+  
+  unless (doc/"span.latitude").empty?
     data.merge!({ 
       :type => 'location',
       :lat  => (doc/"span.latitude").first.inner_html, 
       :long => (doc/"span.longitude").first.inner_html
     })
   else
-    if (doc/"span.bday")
+    unless (doc/"span.bday").empty?
       data.merge!({ 
         :type => 'person',
         :bday => (doc/"span.bday").first.inner_html
